419e85c54d122724a9692e3b5ba81adbf6a9706d
JonesPedD <pedjoness28@gmail.com>
2024-05-11 18:18:16 -0300

    vai porfavor

--- adocao/cards.js
+++ adocao/cards.js
@@ -22,7 +22,7 @@ while (i < 20) {
     novoFlipTras.className = "flip-card-back"
     // nomeando as classes
 
-    let caminhoImg = "/imagensAnimais/animal"
+    let caminhoImg = "../imagens/imagensAnimais/animal"
     caminhoImg = caminhoImg + i
     caminhoImg = caminhoImg + ".jpg"
     //criando o caminho para a imagem
@@ -32,7 +32,7 @@ while (i < 20) {
 
 
 
-    let caminhoInfos = "/infosAnimais/animal"
+    let caminhoInfos = "../imagens/infosAnimais/animal"
     caminhoInfos = caminhoInfos + i
     caminhoInfos = caminhoInfos + ".txt"
     // criando o caminho para as informacoes
